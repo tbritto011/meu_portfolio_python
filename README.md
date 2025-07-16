915fe7ca5bd8ea44dcba69cd3f12c627e17895a2
tbritto011 <allysonteixeirab@gmail.com>
2025-07-15 22:29:06 -0300

    Update and rename README.md to conversor_de_moeda.py
    um codigo que le um determinado valor em real e o converte para dolar, alem de o organizar no fim de forma americana.

--- README.md
+++
@@ -1,2 +0,0 @@
-# meu_portfolio_python
-Portfóĺio com scripts em python desenvolvidos para consolidar fundamentos da linguagem.
---
+++ conversor_de_moeda.py
@@ -0,0 +1,7 @@
+# meu_portfolio_python
+Portfóĺio com scripts em python desenvolvidos para consolidar fundamentos da linguagem.
+
+real = float(input("digite um valor para ser convertido em dolar: "))
+dol = real/5.56
+
+print (f"o valor em reais convertido para dólar é {dol:,.2f}")
